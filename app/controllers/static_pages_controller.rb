d5cc24e6d2779909acda07fa5f60db18617a4123
Chris Siegel <c.siegel1991@gmail.com>
2016-07-09 20:19:45 +0000

    Added a portfolio page to show off my projects.  started styling everything up.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,4 +4,7 @@ class StaticPagesController < ApplicationController
   
   def about
   end
+  
+  def portfolio
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'home' => 'static_pages#home'
   get 'about' =>  'static_pages#about'
+  get 'portfolio' => 'static_pages#portfolio'
   
   get 'application/hello'
 
